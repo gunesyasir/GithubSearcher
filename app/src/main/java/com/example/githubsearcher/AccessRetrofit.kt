6dc40d1370c918df78dd4024cc8b8c40a9f8221e
yasirgunes <myasirgunes@gmail.com>
2022-07-22 15:01:28 +0300

    AccessRetrofit type has changed to object class.

--- app/src/main/java/com/example/githubsearcher/AccessRetrofit.kt
+++ app/src/main/java/com/example/githubsearcher/AccessRetrofit.kt
@@ -6,27 +6,25 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-class AccessRetrofit {
-    companion object {
-        val baseUrl= "https://api.github.com/"
-        private fun getRetrofit(): Retrofit {
-            return Retrofit.Builder()
-                .baseUrl(baseUrl)
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(okHttp())
-                .build()
-        }
+object AccessRetrofit {
+    val baseUrl = "https://api.github.com/"
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttp())
+            .build()
+    }
 
-        fun getInterface():IRetrofit{
-            return getRetrofit().create(IRetrofit::class.java)
-        }
+    fun getInterface(): IRetrofit {
+        return getRetrofit().create(IRetrofit::class.java)
+    }
 
-        private fun okHttp(): OkHttpClient {
-            val logging = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-            val client = OkHttpClient.Builder()
-                .addInterceptor(logging)
-                .build()
-            return client
-        }
+    private fun okHttp(): OkHttpClient {
+        val logging = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+        return client
     }
 }
