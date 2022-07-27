ebf9188a72674c4b520c91bc2b5e70ee82aff683
yasirgunes <myasirgunes@gmail.com>
2022-07-27 20:45:49 +0300

    A viewmodel class has been created and business logic was put in it.

---
+++ app/src/main/java/com/example/githubsearcher/MainViewModel.kt
@@ -0,0 +1,51 @@
+package com.example.githubsearcher
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainViewModel(): ViewModel(){
+     var liveDataResult = MutableLiveData<List<CommonModel>>()
+
+    fun listItems(searchedItem: String){
+        listUsers(searchedItem)
+    }
+     private fun listUsers(searchedItem: String) {
+         var userResult: List<CommonModel>
+        val retrofitObject = AccessRetrofit.getInterface()
+        retrofitObject.getUsersByName(searchedItem).enqueue(object : Callback<UserResponse> {
+            override fun onResponse(call: Call<UserResponse>, response: Response<UserResponse>) {
+                if (response.body() != null) {
+                    userResult = (response.body()!!.items as List<CommonModel>)
+                    listRepos(searchedItem, userResult)
+                }
+            }
+            override fun onFailure(call: Call<UserResponse>, t: Throwable) {
+                Log.e("RetrofitRequestError", t.toString())
+            }
+        })
+    }
+    private fun listRepos(searchedItem: String, userResult: List<CommonModel>) {
+        var repoResult: List<CommonModel>
+        var commonResult: List<CommonModel>
+        val obj = AccessRetrofit.getInterface()
+        obj.getReposByName(searchedItem).enqueue(object : Callback<RepoResponse> {
+            override fun onResponse(call: Call<RepoResponse>, response: Response<RepoResponse>) {
+                if (response.body() != null) {
+                    if (response.isSuccessful) {
+                        repoResult = response.body()!!.items as List<CommonModel>
+                        commonResult = userResult + repoResult
+                        liveDataResult.value = commonResult
+
+                    } else Log.e("ResponseError", "Response is not successfull!!")
+                }
+            }
+            override fun onFailure(call: Call<RepoResponse>, t: Throwable) {
+                Log.e("ResponseFailure", t.toString())
+            }
+        })
+    }
+}
