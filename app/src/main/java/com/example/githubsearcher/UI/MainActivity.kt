56e24f07195bab1fdab059fe7456c6358e0bdce1
yasirgunes <myasirgunes@gmail.com>
2022-08-10 19:38:21 +0300

    Empty query issue has been solved.

--- app/src/main/java/com/example/githubsearcher/UI/MainActivity.kt
+++ app/src/main/java/com/example/githubsearcher/UI/MainActivity.kt
@@ -3,9 +3,10 @@ package com.example.githubsearcher.UI
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
+import android.view.inputmethod.EditorInfo
 import android.widget.Toast
-import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -19,52 +20,51 @@ class MainActivity : AppCompatActivity(), MainAdapter.RecyclerViewItemClickListe
     private val mainViewModel: MainViewModel by lazy {
         ViewModelProvider(this@MainActivity).get(MainViewModel::class.java)
     }
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setViews()
-            mainViewModel.liveDataResult.observe(this@MainActivity, Observer {
-                binding.recyclerView.adapter = MainAdapter(mainViewModel.liveDataResult.value!!, this@MainActivity)
-            })
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setViews()
+        mainViewModel.liveDataResult.observe(this@MainActivity, Observer {
+            binding.recyclerView.adapter = MainAdapter(mainViewModel.liveDataResult.value!!, this@MainActivity)
+        })
+    }
+
+    private fun setViews(){
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        linearLayoutManager = LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = linearLayoutManager
+        binding.root.clearFocus()
+        binding.recyclerView.setHasFixedSize(true)
+        binding.editText.setOnEditorActionListener { textView, i, keyEvent ->
+            if (i == EditorInfo.IME_ACTION_SEARCH){
+                performSearch()
+                return@setOnEditorActionListener true
+            }
+            false
         }
+        binding.searchButton.setOnClickListener { performSearch() }
+    }
 
-        private fun setViews(){
-            binding = ActivityMainBinding.inflate(layoutInflater)
-            setContentView(binding.root)
-            linearLayoutManager = LinearLayoutManager(this)
-            binding.recyclerView.layoutManager = linearLayoutManager
-            binding.root.clearFocus()
-            binding.recyclerView.setHasFixedSize(true)
-            performSearch()
-        }
-        private fun performSearch(){
-            binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-
-                override fun onQueryTextSubmit(query: String?): Boolean {
-                    if (query != null) {
-                        mainViewModel.listItems(query)
-                    } else {
-                        Log.e("QueryTextError", "Query text is null!")
-                        Toast.makeText(
-                            this@MainActivity,
-                            "You need to type some words!",
-                            Toast.LENGTH_LONG
-                        ).show()
-                    }
-                    return true
-                }
-
-                override fun onQueryTextChange(newText: String?): Boolean {
-                    return false
-                }
-            })
-        }
-
-        override fun onItemClick(item: CommonModel) {
-            val intent = Intent(this@MainActivity, DetailActivity::class.java)
-            intent.putExtra("CommonObject", item)
-            this@MainActivity.startActivity(intent)
+    private fun performSearch(){
+        val query = binding.editText.text.toString()
+        if (TextUtils.getTrimmedLength(query)!=0) {
+            mainViewModel.listItems(query)
+        } else{
+            Log.e("QueryTextError", "Query text is null!")
+            Toast.makeText(
+                this@MainActivity,
+                "You need to type some words!",
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
+
+    override fun onItemClick(item: CommonModel) {
+        val intent = Intent(this@MainActivity, DetailActivity::class.java)
+        intent.putExtra("CommonObject", item)
+        this@MainActivity.startActivity(intent)
+    }
+}
 
 
 
